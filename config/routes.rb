47f2653a68fcb941e4cbbf04d7acf2aa237d66a0
max <hchs200771@gmail.com>
2022-10-27 23:42:13 +0800

    後台檔案架構

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :products, only: [:index, :show]
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  namespace :admin do
+    root 'products#index' # /admin/ 後台根路徑
+    resources :products
+  end
 end
